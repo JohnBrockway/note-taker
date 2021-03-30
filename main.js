7c04a0559b18e9c4a93ca4a0ffcc93d4f225204d
JohnBrockway <john.c.brockway@gmail.com>
2021-03-30 12:16:13 -0400

    Add more basic SQL methods

--- main.js
+++ main.js
@@ -19,13 +19,10 @@ function createWindow() {
 }
 
 function maybeSetUpDatabase() {
-    db.serialize(function () {
-        db.run('CREATE TABLE IF NOT EXISTS Worlds (Name TEXT, SessionCount INTEGER)');
-        db.run('CREATE TABLE IF NOT EXISTS Sessions (Name TEXT, Number INTEGER)');
-        db.run('CREATE TABLE IF NOT EXISTS Items (Name TEXT, Alias INTEGER, World INTEGER)');
-        db.run('CREATE TABLE IF NOT EXISTS Notes (Text TEXT, Session INTEGER, RelatedItems TEXT)');
-        db.run('INSERT INTO Notes (Text, Session, RelatedItems) VALUES ("This is a note",1,"/1/3/")');
-    });
+    // TODO send 'ready' ping when these are all set up, block app function until ping
+    db.run('CREATE TABLE IF NOT EXISTS Worlds (ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT)');
+    db.run('CREATE TABLE IF NOT EXISTS Items (ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Alias INTEGER, World INTEGER, IsSession INTEGER, SessionOrder INTEGER)');
+    db.run('CREATE TABLE IF NOT EXISTS Notes (ID INTEGER PRIMARY KEY AUTOINCREMENT, Text TEXT, RelatedItems TEXT)');
 }
 
 app.whenReady().then(() => {
@@ -48,4 +45,57 @@ ipcMain.on('db-get-notes-for-item', (event, itemId) => {
     stmt.all([itemId], function(err, rows) {
         event.sender.send('db-get-notes-for-item-response', JSON.stringify(rows));
     });
+});
+
+ipcMain.on('db-get-items-for-world', (event, worldId) => {
+    const stmt = db.prepare('SELECT * FROM Items WHERE World=?');
+    stmt.all([worldId], function(err, rows) {
+        event.sender.send('db-get-items-for-world-response', JSON.stringify(rows));
+    });
+});
+
+ipcMain.on('db-get-worlds', (event) => {
+    db.all('SELECT * FROM Worlds', function(err, rows) {
+        event.sender.send('db-get-worlds-response', JSON.stringify(rows));
+    });
+});
+
+ipcMain.on('db-add-note', (event, note) => { 
+    const stmt = db.prepare('INSERT INTO Notes (Text, RelatedItems) VALUES (?,?,?)');
+    stmt.run([note.text, note.relatedItems]);
+});
+
+ipcMain.on('db-add-item', (event, item) => { 
+    const stmt = db.prepare('INSERT INTO Items (Name, Alias, World, IsSession) VALUES (?,?,?,?)');
+    stmt.run([item.name, item.alias, item.world, 0]);
+});
+
+ipcMain.on('db-add-session', (event, session) => { 
+    const stmt = db.prepare('INSERT INTO Items (Name, Alias, World, IsSession, SessionOrder) VALUES (?,?,?,?,?)');
+    stmt.run([session.name, session.alias, session.world, 1, session.sessionOrder]);
+});
+
+ipcMain.on('db-add-world', (event, world) => { 
+    const stmt = db.prepare('INSERT INTO Worlds (Name) VALUES (?)');
+    stmt.run([world.name]);
+});
+
+ipcMain.on('db-update-note', (event, note) => {
+    const stmt = db.prepare('UPDATE Notes SET Text=?, RelatedItems=? WHERE ID=?');
+    stmt.run([note.text, note.relatedItems, note.id]);
+});
+
+ipcMain.on('db-update-item', (event, item) => { 
+    const stmt = db.prepare('UPDATE Items SET Name=?, Alias=?, World=?, IsSession=? WHERE ID=?');
+    stmt.run([item.name, item.alias, item.world, 0, item.id]);
+});
+
+ipcMain.on('db-update-session', (event, session) => { 
+    const stmt = db.prepare('UPDATE Items SET Name=?, Alias=?, World=?, IsSession=?, SessionOrder=? WHERE ID=?');
+    stmt.run([session.name, session.alias, session.world, 1, session.sessionOrder, session.id]);
+});
+
+ipcMain.on('db-update-world', (event, world) => { 
+    const stmt = db.prepare('UPDATE Worlds SET Name=? WHERE ID=?');
+    stmt.run([world.name, world.id]);
 });
--- scripts/preload.js
+++ scripts/preload.js
@@ -4,7 +4,19 @@ contextBridge.exposeInMainWorld(
     'electron',
     {
         getNotesForItem: (itemId) => ipcRenderer.send('db-get-notes-for-item', itemId),
-        getNotesForItemResponse: (callback) => ipcRenderer.on('db-get-notes-for-item-response', callback)
+        getNotesForItemResponse: (callback) => ipcRenderer.on('db-get-notes-for-item-response', callback),
+        getItemsForWorld: (worldId) => ipcRenderer.send('db-get-items-for-world', worldId),
+        getItemsForWorldResponse: (callback) => ipcRenderer.on('db-get-items-for-world-response', callback),
+        getWorlds: () => ipcRenderer.send('db-get-worlds'),
+        getWorldsResponse: (callback) => ipcRenderer.on('db-get-worlds-response', callback),
+        addNote: (note) => ipcRenderer.send('db-add-note', note),
+        addItem: (item) => ipcRenderer.send('db-add-item', item),
+        addSession: (session) => ipcRenderer.send('db-add-session', session),
+        addWorld: (world) => ipcRenderer.send('db-add-world', world),
+        updateNote: (note) => ipcRenderer.send('db-update-note', note),
+        updateItem: (item) => ipcRenderer.send('db-update-item', item),
+        updateSession: (session) => ipcRenderer.send('db-update-session', session),
+        updateWorld: (world) => ipcRenderer.send('db-update-world', world),
     }
 );
 
